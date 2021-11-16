0f2da9ef31837aebe3b5a38e6272ca3ce120a0f9
AibtGlobal <app.developer@aibtglobal.edu.au>
2021-11-16 23:29:18 +0100

    revert search page grid text layout

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,10 +56,10 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "icons/Icon-192.png": "e9496214e3d35ee15dc86f0644235843",
 "icons/Icon-512.png": "c63a380942b450a7975729a6b563fa82",
-"index.html": "49c5a80e7799109d78b76a6cedaa9dd2",
-"/": "49c5a80e7799109d78b76a6cedaa9dd2",
+"index.html": "f2846b4abcd02dccec19452b42f501b2",
+"/": "f2846b4abcd02dccec19452b42f501b2",
 "logo.png": "2e53123a8ad3bcade4761ea34b23fa29",
-"main.dart.js": "466f0552a9f90ceaf972052ad36c35cc",
+"main.dart.js": "2fd1137562b7a80ee81391538f0abc5f",
 "manifest.json": "99da374e58b3747ed8611ab092577019",
 "version.json": "cf770f007ad447a7bbce1247d6a57d49"
 };
