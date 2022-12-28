658f83cd71ba36196934d72d1bdb9d8b709bdd98
Viskee Consultancy <app.developer@aibtglobal.edu.au>
2022-12-28 23:52:07 +0100

    new release

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,10 +32,10 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "icons/Icon-192.png": "b8c14e5831870749365b6ced192a7070",
 "icons/Icon-512.png": "8585ad6b41e3d882af13a4c859e8c14c",
-"index.html": "ca3813bf4b60e6e0cba6d1f2534e127d",
-"/": "ca3813bf4b60e6e0cba6d1f2534e127d",
+"index.html": "4860049f744199670317024ac93c6e75",
+"/": "4860049f744199670317024ac93c6e75",
 "logo.png": "6e96f59fe0611f70e9d51eba20d317f0",
-"main.dart.js": "3213adcc6fd0c27fbc8c5ebad829c66a",
+"main.dart.js": "86acc516a89d4019721d127561aa1ad2",
 "manifest.json": "8dbfa948e1c39c73bc66762a2ee70e76",
 "version.json": "cf770f007ad447a7bbce1247d6a57d49"
 };
