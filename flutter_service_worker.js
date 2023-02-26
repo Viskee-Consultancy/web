89f82a18b5e3408b804150aa5647d23943223952
Viskee Consultancy <app.developer@aibtglobal.edu.au>
2023-02-26 22:11:28 +0100

    implement NPA school

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "f6fedd86b4cc2ab4eb14dd3e466349ba",
+  "assets/AssetManifest.json": "100518ec7385aa1b5813d07dd7571f33",
 "assets/FontManifest.json": "14daee83a296805922b95e4a1245e4b4",
 "assets/fonts/DMSans-Bold.ttf": "071853031a2175ada019db9e6fd1585c",
 "assets/fonts/DMSans-Regular.ttf": "3e7f038b85daa739336e4a3476c687f2",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/images/2.0x/background.png": "ad5002027a7ac446249246fd18f9ba71",
 "assets/images/3.0x/background.png": "552cf4a76057f0d0119f8477aadccb41",
 "assets/images/aibt.png": "a590e755424511eeaa277290e31dcb5b",
@@ -19,23 +19,27 @@ const RESOURCES = {
 "assets/images/background.png": "b9d7b584088643ef62a974f508aac018",
 "assets/images/logo.png": "8585ad6b41e3d882af13a4c859e8c14c",
 "assets/images/logo_backgroud_white.png": "c8eecb98501c0296e7f57b31962b4443",
+"assets/images/npa.png": "229bef20299c2d46685136df4f01df6c",
+"assets/images/npa.svg": "67a026cb382bf2de7963dfe72b2f4523",
 "assets/images/reach.png": "b82d1d1137564174a079010334db95b5",
 "assets/images/reach.svg": "d88d1869d4a6118e4c4269fb046f2484",
 "assets/images/reach_landscape.png": "b0d758f3290c5c50a654231e3d70b6c3",
 "assets/images/vc_logo_landscape.png": "d92115ab231bfedf7f788be47bcfff4e",
 "assets/images/vc_logo_landscape.svg": "6820d91f7a3b11c497c39895a5ef5066",
-"assets/NOTICES": "011037c9f9503d8bb3cdea6b14b04972",
+"assets/NOTICES": "a72b08b18b5c84456bcdcf7ba02de998",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
-"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
-"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
-"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
+"assets/shaders/ink_sparkle.frag": "29d09a374bfd26c53ff42d016773960a",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "b8c14e5831870749365b6ced192a7070",
 "icons/Icon-512.png": "8585ad6b41e3d882af13a4c859e8c14c",
-"index.html": "f439bc538d3f5508e35349c93a91f4b1",
-"/": "f439bc538d3f5508e35349c93a91f4b1",
+"index.html": "9f95acd28048ef5883799ac21fc08c49",
+"/": "9f95acd28048ef5883799ac21fc08c49",
 "logo.png": "6e96f59fe0611f70e9d51eba20d317f0",
-"main.dart.js": "af0bf86a8e37dd0f0a18469278577025",
+"main.dart.js": "2324fcfd360ddb508ca980204de378ad",
 "manifest.json": "8dbfa948e1c39c73bc66762a2ee70e76",
 "version.json": "cf770f007ad447a7bbce1247d6a57d49"
 };
@@ -43,10 +47,8 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
+  "main.dart.js",
 "index.html",
-"assets/NOTICES",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
@@ -145,9 +147,11 @@ self.addEventListener("fetch", (event) => {
     .then((cache) =>  {
       return cache.match(event.request).then((response) => {
         // Either respond with the cached resource, or perform a fetch and
-        // lazily populate the cache.
+        // lazily populate the cache only if the resource was successfully fetched.
         return response || fetch(event.request).then((response) => {
-          cache.put(event.request, response.clone());
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
           return response;
         });
       })
