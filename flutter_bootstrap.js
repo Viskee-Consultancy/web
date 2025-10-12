0cbe0d802d77668398a3175fae4296f78f391856
Zelong CHEN <Zelong.CHEN+amadeus@amadeus.com>
2025-10-13 00:24:35 +0200

    implement new school pivot

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2379672874"
+    serviceWorkerVersion: "243597072"
   }
 });
